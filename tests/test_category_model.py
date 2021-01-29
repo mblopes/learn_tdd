56389c785e36fb93debeb172d4eaf1ff9bde030a
Matheus B. Lopes <matheus.bachiste@olist.com>
2021-01-29 16:09:47 -0300

    Updated test category model

--- tests/test_category_model.py
+++ tests/test_category_model.py
@@ -6,17 +6,42 @@ from models.base_model import BaseModel
 
 
 class TestCategoryModel:
-    def test_name_none(self):
+    def test_category_instance(self):
+        category = Category('test name', 'test descirption')
+        assert isinstance(category, Category)
+        assert isinstance(category, BaseModel)
+
+    def test_name_isinstance_str(self):
         try:
-            category = Category(None, 'Category test')
-            raise NotImplementedError('Not implemented exception')
+            Category(None, 'test description')
+            raise NotImplementedError('Not implemented error')
         except Exception as error:
             assert isinstance(error, TypeError)
-    
-    def test_name_blank(self):
+
+    def test_name_blank_spaces(self):
         try:
-            category = Category(' ', 'Category test')
-            raise NotImplementedError('Not implemented exception')
+            Category(' ', 'test description')
+            raise NotImplementedError('Not implemented error')
         except Exception as error:
             assert isinstance(error, ValueError)
-        
+
+    def test_name_too_big(self):
+        try:
+            Category('test name'*100, 'test description')
+            raise NotImplementedError('Not implemented error')
+        except Exception as error:
+            assert isinstance(error, ValueError)
+
+    def test_description_isinstance_str(self):
+        try:
+            Category('test name', None)
+            raise NotImplementedError('Not implemented error')
+        except Exception as error:
+            assert isinstance(error, TypeError)
+
+    def test_description_too_big(self):
+        try:
+            Category('test name'*100, 'test description')
+            raise NotImplementedError('Not implemented error')
+        except Exception as error:
+            assert isinstance(error, ValueError)
