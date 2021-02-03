ac5fac3ea9497e3cb7d3ddbbec403f443059fbf2
josue-avila <josue.avila@olist.com>
2021-02-03 13:13:22 -0300

    refactoring category model tests to use pytest fixtures

--- tests/tests_models/test_category_model.py
+++ tests/tests_models/test_category_model.py
@@ -1,49 +1,65 @@
-import sys
-sys.path.append('.')
-
 from models.category_model import Category
 from models.base_model import BaseModel
 import pytest
 
+
 class TestCategoryModel:
+    @pytest.fixture
+    def category_instance(self):
+        category = Category('category name', 'description')
+        return category
+
+    @pytest.mark.parametrize("name, description", [
+        ('test name', 'test description'),
+        ('t', ''),
+        ('t'*100, 't'*150)
+    ])
+    def test_category_instance(self, category_instance, name, description):
+        category_instance.name = name
+        category_instance.description = description
+        assert isinstance(category_instance, Category)
+        assert isinstance(category_instance, BaseModel)
+
+    def test_category_args(self, category_instance):
+        category_instance.name = 'test name'
+        category_instance.description = 'test description'
+        assert category_instance.name is 'test name'
+        assert category_instance.description is 'test description'
+
     @pytest.mark.parametrize("name", [
         None, 5, 5.0, [1, 2, 3],
         ('a', 'b', 'c'),
         Category('name', 'description'),
         {'name': 'test name',
-        'description': 'test description'}
+         'description': 'test description'}
     ])
-    def test_name_not_instance_str(self, name):
+    def test_name_not_instance_str(self, category_instance, name):
         with pytest.raises(TypeError):
-            Category(name, 'test description')
+            category_instance.name = name
 
-    def test_name_not_instance_str(self):
-        with pytest.raises(TypeError):
-            Category(None, 'test description')
+    def test_name_blank_spaces(self, category_instance):
+        with pytest.raises(ValueError):
+            category_instance.name = ''
 
-    def test_name_min_len(self):
-        name = 'N'
-        description = ''
-        category = Category(name, description)
-        assert category.name is name        
-
-    @pytest.mark.parametrize("name, description", [
-        (10, 'test description'),
-        (10.5, 'test description'),
-        (False, 'test description')
+    @pytest.mark.parametrize("description", [
+        None, 5, 5.0, [1, 2, 3],
+        ('a', 'b', 'c'),
+        Category('name', 'description'),
+        {'name': 'test name',
+         'description': 'test description'}
     ])
-    def test_name_blank_spaces(self, name, description):
-        with pytest.raises(ValueError):
-            category = Category('', 'test description')
-
-    def test_name_too_big(self):
-        with pytest.raises(ValueError):
-            category = Category('test name'*100, 'test description')
-
-    def test_description_not_none(self):
+    def test_description_not_instance_str(self, category_instance, description):
         with pytest.raises(TypeError):
-            category = Category('test name', None)
+            category_instance.description = description
 
-    def test_description_too_big(self):
+    def test_name_too_big(self, category_instance):
         with pytest.raises(ValueError):
-            category = Category('test name', 'test description'*200)
+            category_instance.name = 'test name'*100
+
+    def test_description_not_none(self, category_instance):
+        with pytest.raises(TypeError):
+            category_instance.description = None
+
+    def test_description_too_big(self, category_instance):
+        with pytest.raises(ValueError):
+            category_instance.description = 'test description'*150
