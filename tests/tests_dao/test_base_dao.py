b88c51161897c86381e4e64a308fed9edecaeece
josue-avila <josue.avila@olist.com>
2021-02-04 08:57:36 -0300

    fixing base dao tests

--- tests/tests_dao/test_base_dao.py
+++ tests/tests_dao/test_base_dao.py
@@ -6,8 +6,13 @@ import pytest
 
 class TestBaseDao:
     @pytest.fixture
-    def base_dao_instance(self):
+    def base_dao_model_instance(self):
         return BaseDao(Category)
 
-    def test_base_dao_instance(self, base_dao_instance):
-        assert isinstance(base_dao_instance, BaseDao)
+    @pytest.fixture
+    def base_dao_base_model_instance(self):
+        return BaseDao(BaseModel)
+
+    def test_base_dao_instance(self, base_dao_model_instance, base_dao_base_model_instance):
+        assert isinstance(base_dao_model_instance, BaseDao)
+        assert isinstance(base_dao_base_model_instance, BaseDao)
