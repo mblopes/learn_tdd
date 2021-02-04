337f4294eaf7c838bfba17f50e4bb98b65c939a8
josue-avila <josue.avila@olist.com>
2021-02-04 11:38:49 -0300

    removing DB credantials from dao/session.py

--- dao/session.py
+++ dao/session.py
@@ -7,17 +7,12 @@ from sqlalchemy.orm import sessionmaker
 class Session:
     def __init__(self):
         load_dotenv()
-        connector = 'mysql+pymysql'
-        host = 'mysql09-farm15.uni5.net'
-        user = 'topskills17'
-        password = 'ButecoOlist21'
-        dbname = 'topskills17'
-        '''connector = os.getenv('CONNECTOR')
-        host = os.getenv('HOST')
-        user = os.getenv('USER')
-        password = os.getenv('PASSWORD')
-        dbname = os.getenv('DATABASE')'''
-        self.__conn_string = f'{connector}://{user}:{password}@{host}:3306/{dbname}'
+        connector = os.getenv('DB_CONNECTOR')
+        host = os.getenv('DB_HOST')
+        user = os.getenv('DB_USER')
+        password = os.getenv('DB_PASSWORD')
+        dbname = os.getenv('DB_DATABASE')
+        self.__conn_string = f"{connector}://{user}:{password}@{host}:3306/{dbname}"
 
     def __enter__(self):
         self.__engine = create_engine(self.__conn_string)
@@ -28,3 +23,5 @@ class Session:
     def __exit__(self, type, value, traceback):
         self.__session.close()
         self.__engine.dispose()
+
+Session()
