b8192c2effc89caa5c7712ef1ce4ffd8eec0c436
Matheus B. Lopes <matheus.bachiste@olist.com>
2021-02-05 09:15:56 -0300

    Category register created

--- view/web.py
+++ view/web.py
@@ -1,8 +1,9 @@
 import sys
 sys.path.append('.')
 
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 from controllers.category_controller import CategoryController
+from models.category_model import Category
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
@@ -18,12 +19,25 @@ def category():
     categories = controller.read_all()
     return render_template('list_categories.html', categories=categories)
 
+@app.route('/category/create', methods=['POST', 'GET'])
+def create_category():
+    if request.method == 'POST':
+        controller = CategoryController()
+        name = request.form['name']
+        description = request.form['description']
+        category = Category(name, description)
+        controller.create(category)
+        return redirect('/category')
+    
+    return render_template('create_category.html')
+
 @app.route('/category/delete/<int:id>')
 def delete_category(id):
     controller = CategoryController()
     category = controller.read_by_id(id)
     controller.delete(category)
     return redirect('/category')
+
 
 
 app.run(debug=True)
